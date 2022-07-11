2471550a708a15ca78308bee82bcd2d2007b9ac6
Bruna Andrade <brunansandrade@gmail.com>
2022-07-11 15:49:13 -0300

    organiza caminhos das urls e imprime olá mundo no index

--- encurtador/models.py
+++ encurtador/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Links(models.Model):
     link_redirecionado = models.URLField
     link_personalizado = models.CharField(max_length=8)
--- encurtador/views.py
+++ encurtador/views.py
@@ -1,3 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('olá mundo')
