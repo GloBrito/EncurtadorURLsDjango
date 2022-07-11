d948f2c3d309065345740ba193327d141e6826e0
Bruna Andrade <brunansandrade@gmail.com>
2022-07-11 15:18:49 -0300

    cria models e roda migrate

--- encurtador/models.py
+++ encurtador/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Links(models.Model):
+    link_redirecionado = models.URLField
+    link_personalizado = models.CharField(max_length=8)
+
+    def __str__(self) -> str:
+        return self.link_personalizado
