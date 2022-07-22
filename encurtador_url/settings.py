91f4a34faf6e14aa988a55bbbfff0d85990c4d23
evandropcs <evandropcs@hotmail.com>
2022-07-22 13:30:42 -0300

    Seta variavel debug para False por padrão

--- encurtador_url/settings.py
+++ encurtador_url/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='set-a-secure-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS", default="*", cast=lambda v: [s.strip() for s in v.split(",")]
